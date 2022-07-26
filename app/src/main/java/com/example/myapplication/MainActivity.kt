0f7954a1e35521f837f89a107ea19975d80f6d65
anreh <anurehka@gmail.com>
2022-07-26 09:31:01 +0800

    Commit 20220719 1029 - Added use interactivity - editText added - Update button added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImg: ImageView
+    lateinit var numberText: TextView
+    lateinit var editPlayerName: EditText
+    lateinit var playerNameTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImg = findViewById(R.id.diceImg)
+        numberText = findViewById(R.id.numberText)
+        editPlayerName = findViewById(R.id.editPlayerName)
+        playerNameTV = findViewById(R.id.playerName)
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener{ rollDice()}
+        val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)
+        updateButton.setOnClickListener{updatePlayerName(it)}
     }
+
+    private fun rollDice(){
+        val randomNum= (1..6).random()
+        //val numberText: TextView = findViewById(R.id.numberText)
+        //val diceImg: ImageView = findViewById(R.id.diceImg)
+        numberText.text = randomNum.toString()
+
+        val imgSrc = when (randomNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImg.setImageResource(imgSrc)
+    }
+    private fun updatePlayerName(view: View){
+        playerNameTV.text = editPlayerName.text
+
+        editPlayerName.setText(" ")
+        editPlayerName.clearFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
