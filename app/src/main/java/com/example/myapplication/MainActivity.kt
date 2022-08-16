ba5d9bb997d7335078eb385b6f9432fab005e750
anreh <anurehka@gmail.com>
2022-08-16 10:54:40 +0800

    20220816 -method:binding data -roll the dick with totalScore -data class

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,44 +9,39 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImg: ImageView
     lateinit var numberText: TextView
     lateinit var editPlayerName: EditText
     lateinit var playerNameTV: TextView
+    lateinit var binding: ActivityMainBinding
+    private val gameInfo: MyName = MyName("Leng", "0")
+    private var totalScore: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        diceImg = findViewById(R.id.diceImg)
-        numberText = findViewById(R.id.numberText)
-        editPlayerName = findViewById(R.id.editPlayerName)
-        playerNameTV = findViewById(R.id.playerName)
-        val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.setOnClickListener{ rollDice()}
-        val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)
-        updateButton.setOnClickListener{updatePlayerName(it)}
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        /* diceImg = findViewById(R.id.diceImg)
+         numberText = findViewById(R.id.numberText)
+         editPlayerName = findViewById(R.id.editPlayerName)
+         playerNameTV = findViewById(R.id.playerName) */
+        /*  val rollButton: Button = findViewById(R.id.rollButton) */
+        binding.gameInfo = gameInfo
+        binding.rollButton.setOnClickListener { rollDice() }
+/* val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn) */
+        binding.updatePlayerNameBtn.setOnClickListener { updatePlayerName(it) }
     }
 
-    private fun rollDice(){
-        val randomNum= (1..6).random()
-        //val numberText: TextView = findViewById(R.id.numberText)
-        //val diceImg: ImageView = findViewById(R.id.diceImg)
-        numberText.text = randomNum.toString()
-
-        val imgSrc = when (randomNum){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+    private fun updatePlayerName(view: View) {
+        binding.apply {
+            gameInfo?.playerName = binding.editPlayerName.text.toString()
+            invalidateAll()
         }
-        diceImg.setImageResource(imgSrc)
-    }
-    private fun updatePlayerName(view: View){
-        playerNameTV.text = editPlayerName.text
+
+// binding.playerNameTV.text = editPlayerName.text
 
         editPlayerName.setText(" ")
         editPlayerName.clearFocus()
@@ -54,5 +49,24 @@ class MainActivity : AppCompatActivity() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
+
+    private fun rollDice() {
+        val randomNum = (1..6).random()
+        //val numberText: TextView = findViewById(R.id.numberText)
+        //val diceImg: ImageView = findViewById(R.id.diceImg)
+        totalScore += randomNum
+        binding.numberText.text = totalScore.toString()
+
+        val imgSrc = when (randomNum) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.diceImg.setImageResource(imgSrc)
+    }
+
 
 }
